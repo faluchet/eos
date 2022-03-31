e8e63db83603fccfc1c5ad7a864fbe59a5509649
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2022-03-31 19:35:09 +0200

    MGM: GroupDrainer: some WIP skeleton for picking files

--- mgm/GroupDrainer.cc
+++ mgm/GroupDrainer.cc
@@ -3,6 +3,7 @@
 #include "mgm/XrdMgmOfs.hh"
 #include "mgm/Master.hh"
 #include "mgm/groupbalancer/GroupsInfoFetcher.hh"
+#include "common/utils/ContainerUtils.hh"
 
 namespace eos::mgm {
 
@@ -10,8 +11,8 @@ using group_balancer::eosGroupsInfoFetcher;
 using group_balancer::GroupStatus;
 
 GroupDrainer::GroupDrainer(std::string_view spacename) : mSpaceName(spacename),
-                                                         mEngine(std::make_unique<group_balancer::StdDrainerEngine>())
-
+                                                         mEngine(std::make_unique<group_balancer::StdDrainerEngine>()),
+                                                         numTx(10000)
 {
   mThread.reset(&GroupDrainer::GroupDrain, this);
 }
@@ -22,7 +23,7 @@ GroupDrainer::~GroupDrainer()
 }
 
 void
-GroupDrainer::GroupDrain(ThreadAssistant& assistant)
+GroupDrainer::GroupDrain(ThreadAssistant& assistant) noexcept
 {
   eosGroupsInfoFetcher fetcher(mSpaceName,
                                [](GroupStatus s) {
@@ -36,27 +37,122 @@ GroupDrainer::GroupDrain(ThreadAssistant& assistant)
       continue;
     }
 
-    if (IsUpdateNeeded()) {
+    pruneTransfers();
+
+    if (isTransfersFull()) {
+      // We are currently full, wait for a few seconds before pruning & trying
+      // again
+      eos_info("msg=\"transfer queue full, pausing before trying again\"");
+      assistant.wait_for(std::chrono::seconds(10));
+      continue;
+    }
+
+    if (isUpdateNeeded(mLastUpdated)) {
       mEngine->populateGroupsInfo(fetcher.fetch());
     }
 
+    if (!mEngine->canPick()) {
+      eos_info("msg=\"Cannot pick, Empty source or target groups, check status "
+               "if this is not expected\"");
+      assistant.wait_for(std::chrono::seconds(10));
+      continue;
+    }
+
+    prepareTransfers();
   }
 }
+
 bool
-GroupDrainer::IsUpdateNeeded(bool force)
+GroupDrainer::isUpdateNeeded(std::chrono::time_point<std::chrono::steady_clock>& tp,
+                             bool force)
 {
   using namespace std::chrono_literals;
   auto now = chrono::steady_clock::now();
   if (force) {
-    mLastUpdated = now;
+    tp = now;
     return true;
   }
   auto elapsed = chrono::duration_cast<chrono::seconds>(now - mLastUpdated);
   if (elapsed > mCacheExpiryTime) {
-    mLastUpdated = now;
+    tp = now;
     return true;
   }
   return false;
 }
 
+// Prune all transfers which are done by Converter, since the converter will
+// pop entries off FidTracker once done, this should give us an idea of our
+// queued transfers being actually realized
+void
+GroupDrainer::pruneTransfers()
+{
+  auto prune_count = eos::common::erase_if(mTransfers, [](const auto& p) {
+    return !gOFS->mFidTracker.HasEntry(p.first);
+  });
+
+  eos_info("msg=\"pruned %ul transfers, transfers in flight=%ul\"",
+           prune_count, mTransfers.size());
+}
+
+void
+GroupDrainer::prepareTransfers()
+{
+  uint64_t allowed_tx = numTx - mTransfers.size();
+  for (uint64_t i = 0; i < allowed_tx; ++i) {
+    prepareTransfer(i);
+  }
+}
+
+void
+GroupDrainer::prepareTransfer(uint64_t index)
+{
+  auto [grp_drain_from, grp_drain_to] = mEngine->pickGroupsforTransfer(index);
+
+  if (grp_drain_from.empty() || grp_drain_to.empty()) {
+    eos_static_info("msg=\"engine gave us empty groups skipping\"");
+    return;
+  }
+
+  auto fsids = mDrainFsMap.find(grp_drain_from);
+  if (fsids == mDrainFsMap.end() || isUpdateNeeded(mDrainMapLastUpdated)) {
+    std::tie(fsids, std::ignore) = mDrainFsMap.insert_or_assign(grp_drain_from,
+                                                                FsidsinGroup(grp_drain_from));
+  }
+
+  auto fsid = eos::common::pickIndexRR(fsids->second, index);
+  auto fids = mCacheFileList.find(fsid);
+  if (fids == mCacheFileList.end()) {
+    populateFids(fsid);
+  }
+
+  if (auto fids= mCacheFileList.find(fsid);
+      fids != mCacheFileList.end()) {
+    if (fids->second.size() > 0) {
+      scheduleTransfer(fids->second.back(), grp_drain_from, grp_drain_to);
+      fids->second.pop_back();
+    }
+  } else {
+    eos_info("\"msg=couldn't find files in fsid=%d", fsid);
+  }
+}
+
+//! TODO: implement the following!!!!
+void
+GroupDrainer::scheduleTransfer(eos::common::FileId::fileid_t fid,
+                               std::string_view src_grp,
+                               std::string_view tgt_grp)
+{
+}
+
+void
+GroupDrainer::populateFids(eos::common::FileSystem::fsid_t fsid)
+{
+}
+
+std::vector<eos::common::FileSystem::fsid_t>
+FsidsinGroup(std::string_view groupname)
+{
+
+  return {};
+}
 } // eos::mgm
--- mgm/GroupDrainer.hh
+++ mgm/GroupDrainer.hh
@@ -25,6 +25,9 @@
 #include <string_view>
 #include "common/AssistedThread.hh"
 #include "common/Logging.hh"
+#include "common/FileId.hh"
+#include "common/FileSystem.hh"
+#include <vector>
 
 namespace eos::mgm {
 
@@ -32,6 +35,8 @@ namespace group_balancer {
 class BalancerEngine;
 } // group_balancer
 
+std::vector<eos::common::FileSystem::fsid_t>
+FsidsinGroup(std::string_view groupname);
 
 
 class GroupDrainer: public eos::common::LogId {
@@ -39,13 +44,38 @@ public:
   GroupDrainer(std::string_view spacename);
   ~GroupDrainer();
   void GroupDrain(ThreadAssistant& assistant) noexcept;
-  bool IsUpdateNeeded(bool force=false);
+
+  bool isUpdateNeeded(std::chrono::time_point<std::chrono::steady_clock>& tp,
+                      bool force=false);
+  void pruneTransfers();
+  bool isTransfersFull() const {
+    return mTransfers.size() > numTx;
+  }
+
+  void prepareTransfers();
+  void prepareTransfer(uint64_t index);
+  void scheduleTransfer(eos::common::FileId::fileid_t fid,
+                        std::string_view src_grp, std::string_view tgt_grp);
+  void populateFids(eos::common::FileSystem::fsid_t fsid);
 private:
   std::chrono::time_point<std::chrono::steady_clock> mLastUpdated;
+  std::chrono::time_point<std::chrono::steady_clock> mDrainMapLastUpdated;
   std::chrono::seconds mCacheExpiryTime {300};
   std::string mSpaceName;
   AssistedThread mThread;
   std::unique_ptr<group_balancer::BalancerEngine> mEngine;
+  uint32_t numTx; // < Max no of transactions to keep in flight
+  //! map tracking scheduled transfers, will be cleared periodically
+  //! TODO: use a flat_map structure here, we are usually size capped to ~10K
+  std::unordered_map<eos::common::FileId::fileid_t, std::string> mTransfers;
+
+  //! a map holding the current list of FSes in the draining groups
+  //! this is unlikely to have more than a single digit number of keys..maybe a
+  //! a vector of pairs might be ok?
+  std::map<std::string, std::vector<common::FileSystem::fsid_t>> mDrainFsMap;
+  std::map<common::FileSystem::fsid_t,
+           std::vector<common::FileId::fileid_t>> mCacheFileList;
 };
+
 
 } // namespace eos::mgm
